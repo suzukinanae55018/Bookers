72972d32a2421edbac5d75b043172266ce2f93a0
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-06-27 06:55:03 +0000

    [Add]日本語化追記

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module Bookers
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.paths.add "lib", eager_load: true
+    config.i18n.default_locale = :ja
   end
 end
